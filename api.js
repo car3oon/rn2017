976ad2d3ff4a074a5439b0bcebecf1588b093006
Łukasz Węgiel <lukasz@mirumee.com>
2017-03-21 14:47:53 +0100

    Add extra data

--- api.js
+++ api.js
@@ -1,7 +1,19 @@
 const people = [
-  { name: 'Nader', age: 36 },
-  { name: 'Amanda', age: 24 },
-  { name: 'Jason', age: 44 }
+  {
+    name: 'Nader',
+    age: 36,
+    bio: 'Quam repudiandae dolor sint quasi consequatur, aliquam exercitationem, magni porro! Dolor ullam doloreque cum vel reprehenderit tempore?'
+  },
+  {
+    name: 'Amanda',
+    age: 24,
+    bio: 'Minima repellat id nemo! Culpa pariatur aliquid maiores ratione, incidunt non ea omnis vitae repellat obcaecati.'
+  },
+  {
+    name: 'Jason',
+    age: 44,
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam ullam, ipsam fugiat corporis quae saepe officiis repudiandae modi.'
+  }
 ]
 
 export default () => {
